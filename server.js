19121686c2f319b89bd0a87325ebff252639b9f1
WebCS2 <boomsstudiosalt@gmail.com>
2026-05-02 20:44:15 +0300

    server.js

--- server.js
+++ server.js
@@ -4,31 +4,34 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server,{ cors:{origin:"*"} });
 
-const io = new Server(server, {
-  cors: { origin: "*" },
-});
+let players = {};
 
-io.on("connection", (socket) => {
-  socket.on("join", (name) => {
-    socket.data.name = name;
-    io.emit("chat", { name: "System", message: name + " joined" });
+io.on("connection", socket => {
+
+  socket.on("join", name=>{
+    players[socket.id] = {x:50,y:50,name};
+    io.emit("players", players);
   });
 
-  socket.on("chat", (data) => {
+  socket.on("move", pos=>{
+    if(players[socket.id]){
+      players[socket.id].x = pos.x;
+      players[socket.id].y = pos.y;
+      io.emit("players", players);
+    }
+  });
+
+  socket.on("chat", data=>{
     io.emit("chat", data);
   });
 
-  socket.on("disconnect", () => {
-    if (socket.data.name) {
-      io.emit("chat", {
-        name: "System",
-        message: socket.data.name + " left",
-      });
-    }
+  socket.on("disconnect", ()=>{
+    delete players[socket.id];
+    io.emit("players", players);
   });
+
 });
 
-server.listen(3001, () => {
-  console.log("Server running on port 3001");
-});
+server.listen(3001, ()=>console.log("Server running"));
